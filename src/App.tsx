7d2a0871c41243b0d2ca1723368a017fa19ebe7a
Asya Yeromina <asyayeromina@gmail.com>
2024-04-06 17:46:11 +0200

    forms lecture result

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,44 @@
-// #region imports 
-import React from 'react';
+// #region imports
+import React, { useState } from "react";
 
-import { Post } from './types/Post';
-import { User } from './types/User';
-import postsFromServer from './api/posts.json';
-import usersFromServer from './api/users.json';
-import { PostForm } from './PostForm';
-import { PostList } from './PostList';
+import { Post } from "./types/Post";
+import postsFromServer from "./api/posts.json";
+import { PostForm } from "./components/PostForm";
+import { PostList } from "./components/PostList";
+import { getUserById } from "./services/user";
 // #endregion
 // #region initialPosts
-function getUserById(userId: number): User | null {
-  return usersFromServer.find(user => user.id === userId)
-    || null;
-}
 
-const initialPosts: Post[] = postsFromServer.map(post => ({
+
+const initialPosts: Post[] = postsFromServer.map((post) => ({
   ...post,
   user: getUserById(post.userId),
 }));
 // #endregion
 
+function getNewPostId(posts: Post[]) {
+  const maxId = Math.max(...posts.map(post => post.id));
+  return maxId + 1;
+};
+
 export const App: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>(initialPosts);
+
+  const addNewPost = (post: Post) => {
+    const newPost = {
+      ...post,
+      id: getNewPostId(posts),
+    };
+
+    setPosts(currentPosts => [...currentPosts, newPost])
+  };
+
   return (
     <div className="section">
       <h1 className="title">Create a post</h1>
 
-      <PostForm />
-      <PostList posts={initialPosts} />
+      <PostForm onSubmit={addNewPost}/>
+      <PostList posts={posts} />
     </div>
   );
 };
